bd8cfbe80929c6e4f4a8832410f4a95272018ce1
Pravin Mane <141111800+Pravin7639@users.noreply.github.com>
2024-05-20 23:51:16 +0530

    Singleton Design Pattern

---
+++ Singleton/DbConnection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class DbConnection
+    {
+        private static DbConnection obj = null;            //static property
+
+        private static object o = new object();
+
+        private static int count = 1;
+
+        private DbConnection() { }                        // private constructor
+
+        public static DbConnection Instance
+        {
+            get
+            {
+                        if (count<=5)
+                        {
+                            obj = new DbConnection();
+                               count++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Only 5 Object Creation is allowed.");
+                        }
+                            return obj;
+
+            }
+        }
+
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DbConnection db1 = DbConnection.Instance;
+            DbConnection db2 = DbConnection.Instance;
+            DbConnection db3 = DbConnection.Instance;
+            DbConnection db4 = DbConnection.Instance;
+            DbConnection db5 = DbConnection.Instance;
+            //DbConnection db6 = DbConnection.Instance;   // not poassible
+
+            //DbConnection db = new DbConnection();  // now this is not possible because of private keyword
+
+            if (db1.Equals(db2) && db2.Equals(db3))
+            {
+                Console.WriteLine("db1, db2 and db3 are equal.");
+            }
+            else
+            {
+                Console.WriteLine("db1, db2 and db3 are not equal.");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
